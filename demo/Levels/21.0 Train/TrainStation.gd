569e74c3a3829f093128fcd09d7c76e533ef1dd4
Dougal Pugson <dougal.pugson@gmail.com>
2020-12-03 22:47:58 -0500

    pretty train

---
+++ demo/Levels/21.0 Train/TrainStation.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+onready var stats = PlayerStats
+onready var player = $YSort/player
+onready var bottomSP = $BottomSP
+
+onready var DialogueHelper = preload("res://Dialogue/DialogueHelper.gd")
+
+func _ready():
+	var player_position = bottomSP.position
+	var orientation = Vector2.UP
+	match stats.spawn_metadata:
+		"bottom":
+			#Jukebox.play_song("res://tunes/lab/background_science.wav")
+			player_position = bottomSP.position
+			orientation = Vector2.UP
+		_:
+			pass
+			
+	stats.spawn_player(
+		player, null, 
+		"../../../PuppyCamera", player_position, orientation)
