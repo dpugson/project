3c672efa291c38afff5db932bd4b762cbcb9290f
Dougal Pugson <dougal.pugson@gmail.com>
2021-10-09 19:56:43 -0400

    Start working on pond scene

--- demo/Levels/2.0 - Forest/OutsideLab_02.gd
+++ demo/Levels/2.0 - Forest/OutsideLab_02.gd
@@ -2,30 +2,28 @@ extends Node2D
 
 onready var stats = PlayerStats
 onready var player = $YSort/player
-onready var labSP = $LabSP
+onready var leftSP = $LeftSP
 onready var rightSP = $RightSP
 onready var DialogueHelper = preload("res://Dialogue/DialogueHelper.gd")
 onready var camera = $PuppyCamera
 
 func _ready():
-	var player_position = labSP.global_position
+	var player_position = leftSP.global_position
 	var orientation = Vector2.RIGHT
 	Jukebox.play_song("res://tunes/forest/forest_theme_lofi.wav")
 	match stats.spawn_metadata:
-		"lab":
-			player_position = labSP.global_position
-			orientation = Vector2.UP
+		"left":
+			player_position = leftSP.global_position
+			orientation = Vector2.LEFT
 		"right":
 			player_position = rightSP.global_position
-			orientation = Vector2.DOWN
+			orientation = Vector2.RIGHT
 	stats.spawn_player(
 		player, null, 
 		"../../../PuppyCamera", player_position, orientation)
 
-
-func _on_PuzzleHouseTZ_transition_triggered():
-	Transition.go_to("res://Levels/1.0 - Lab/GiftShop.tscn", "top")
-
+func _on_LeftTZ_transition_triggered():
+	Transition.go_to("res://Levels/2.0 - Forest/OutsideLab_01.tscn", "right")
 
 func _on_RightTZ_transition_triggered():
-	Transition.go_to("res://Levels/2.0 - Forest/OutsideLab_02.tscn", "left")
+	pass # Replace with function body.
